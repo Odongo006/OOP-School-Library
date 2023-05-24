8b8a3b030544a2eea97310f21cbf2b6a4cb55f7b
Odongo006 <www.henryodongo006@gmail.com>
2023-05-24 06:52:32 +0300

    Corrected linters error

--- person.rb
+++ person.rb
@@ -1,12 +1,19 @@
 class Person
-  attr_accessor :name, :age
-  attr_reader :id
+  attr_reader :id, :name, :age
 
-  def initialize(id, age, name = 'Unknown', parent_permission = true)
+  def initialize(id, age, name: 'Unknown', parent_permission: true)
     @id = id
     @name = name
     @age = age
     @parent_permission = parent_permission
+  end
+
+  def name=(new_name)
+    @name = new_name
+  end
+
+  def age=(new_age)
+    @age = new_age
   end
 
   def can_use_services?
